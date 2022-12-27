38a89c5de38b5ce1670a4f13876fd1f2af3da29b
idoctorvic <loukianova@yahoo.com>
2022-12-27 20:18:47 +0300

    added the code sorting the given array

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] arr = { "2", "werty", "2345", ";-)", "2a", "A-2", "-16"};
+
+PrintArray(arr);
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            if (i == array.Length - 1)
+            {
+                Console.Write($"'{array[i]}'");
+            }
+            else
+            {
+                Console.Write($"'{array[i]}', ");
+            }
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
